b5921189e5abb32e394ea0e3608f464d75688148
daffarian <mdaffarian123@gmail.com>
2024-03-19 01:18:05 +0700

    tidur

--- components/Mobile-Nav.tsx
+++ components/Mobile-Nav.tsx
@@ -31,75 +31,84 @@ export default function MobileNav() {
             {Icons.menu}
           </Button>
         </SheetTrigger>
-        <SheetContent className="overflow-y-auto" side={'left'}>
-          <Author />
-          <div className="mt-10 flex flex-col gap-2">
-            {/* Perulangan Komponen Link */}
-            {links.map((link) => {
-              // Jika Tidak memiliki sublink maka render tanpa komponen accordion
-              if (link.subLinks === null) {
-                return (
-                  <div key={link.id}>
-                    <Link
-                      href={link.href}
-                      onClick={() => setIsOpen(!isOpen)}
-                      className={clsx(
-                        'flex flex-row gap-2 items-center hover:bg-sky-100 hover:text-sky-500 py-4 rounded-md',
-                        {
-                          'bg-sky-100 text-sky-500': link.href === pathname
-                        }
-                      )}
-                    >
-                      {link.icon}
-                      {link.name}
-                    </Link>
-                  </div>
-                );
-              } else {
-                return (
-                  // Render dengan accordion jika memiliki sublink
-                  <Accordion type="single" collapsible key={link.id} defaultValue='item-1'>
-                    <AccordionItem value="item-1">
-                      <AccordionTrigger className="flex flex-row items-center hover:underline gap-2 py-4 rounded-md">
+        <SheetContent
+          className="overflow-y-auto h-full justify-between flex flex-col"
+          side={'left'}
+        >
+          <div>
+            <Author />
+            <div className="mt-10 flex flex-col gap-2 ">
+              {/* Perulangan Komponen Link */}
+              {links.map((link) => {
+                // Jika Tidak memiliki sublink maka render tanpa komponen accordion
+                if (link.subLinks === null) {
+                  return (
+                    <div key={link.id}>
+                      <Link
+                        href={link.href}
+                        onClick={() => setIsOpen(!isOpen)}
+                        className={clsx(
+                          'flex flex-row gap-2 items-center hover:bg-sky-100 hover:text-sky-500 px-2 py-4 rounded-md',
+                          {
+                            'bg-sky-100 text-sky-500': link.href === pathname
+                          }
+                        )}
+                      >
                         {link.icon}
                         {link.name}
-                      </AccordionTrigger>
-                      <AccordionContent>
-                        <div className="flex flex-col gap-1">
-                          {link.subLinks.map((sublink) => (
-                            <>
-                              <hr />
-                              <Link
-                                href={sublink.href}
-                                onClick={() => setIsOpen(!isOpen)}
-                                className={clsx(
-                                  'flex flex-row  pl-10 items-center hover:bg-sky-100 border-slate-200 py-2 rounded-md',
-                                  {
-                                    'bg-sky-100 text-sky-500':
-                                      sublink.href === pathname
-                                  }
-                                )}
-                              >
-                                {sublink.name}
-                              </Link>
-                            </>
-                          ))}
-                        </div>
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                );
-              }
-            })}
+                      </Link>
+                    </div>
+                  );
+                } else {
+                  return (
+                    // Render dengan accordion jika memiliki sublink
+                    <Accordion
+                      type="single"
+                      collapsible
+                      key={link.id}
+                      defaultValue="item-1"
+                    >
+                      <AccordionItem value="item-1">
+                        <AccordionTrigger className="flex flex-row items-center hover:underline gap-2 px-2 py-4 rounded-md">
+                          {link.icon}
+                          {link.name}
+                        </AccordionTrigger>
+                        <AccordionContent>
+                          <div className="flex flex-col gap-1">
+                            {link.subLinks.map((sublink) => (
+                              <>
+                                <hr />
+                                <Link
+                                  href={sublink.href}
+                                  onClick={() => setIsOpen(!isOpen)}
+                                  className={clsx(
+                                    'flex flex-row  pl-10 items-center hover:bg-sky-100 border-slate-200 py-2 rounded-md',
+                                    {
+                                      'bg-sky-100 text-sky-500':
+                                        sublink.href === pathname
+                                    }
+                                  )}
+                                >
+                                  {sublink.name}
+                                </Link>
+                              </>
+                            ))}
+                          </div>
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  );
+                }
+              })}
+            </div>
           </div>
-          <Link href={'/'}>
-          <Button
-            variant={'destructive'}
-            className="inline-flex w-full justify-between mt-10"
-          >
-            Logout{Icons.logout}
-          </Button>
-          
+          <Link href={'/'} className="items-end">
+            <Button
+              variant={'destructive'}
+              className="inline-flex w-full justify-between"
+            >
+              Logout{Icons.logout}
+            </Button>
           </Link>
         </SheetContent>
       </Sheet>

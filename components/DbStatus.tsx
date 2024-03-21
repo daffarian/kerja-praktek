d45be2d877b0c15f9e5bcaee4e343ccd689fa4fa
daffarian <mdaffarian123@gmail.com>
2024-03-21 23:40:16 +0700

    fix db status indication

--- components/DbStatus.tsx
+++ components/DbStatus.tsx
@@ -2,6 +2,13 @@
 import { getStatusDb } from '@/lib/data';
 import { useCallback, useState, useEffect } from 'react';
 import clsx from 'clsx';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
+
 export default function DbStatus() {
   const [status, setStatus] = useState(false);
 
@@ -13,6 +20,7 @@ export default function DbStatus() {
   useEffect(() => {
     const interval = setInterval(() => {
       getStatus();
+      console.log(status);
     }, 10000);
 
     // Membersihkan interval saat komponen di-unmount
@@ -22,10 +30,23 @@ export default function DbStatus() {
   }, [getStatus]);
 
   return (
-    <div
-      className={clsx('w-3 h-3 rounded-full bg-red-500 shadow-inner',{
-        'bg-green-400' : status === true
-      })}
-    ></div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <div
+            className={clsx(
+              'w-3 h-3 rounded-full shadow-inner',
+              {
+                'bg-green-400': status === true
+              },
+              { 'bg-red-500': status === false }
+            )}
+          ></div>
+        </TooltipTrigger>
+        <TooltipContent className='bg-white text-green-400 text-xs border-[1px] border-slate-200'>
+          {status == true ? <p className='text-green-400'>DB Conected</p>: <p className='text-red-500'>DB Conection Lost</p>}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }

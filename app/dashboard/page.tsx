97a3c3581c7ec7b004c5960ab5da661d815f790d
daffarian <mdaffarian123@gmail.com>
2024-03-25 23:42:44 +0700

    fix bug nav asem

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,32 +9,34 @@ export default async function Page() {
   const statusUp = await fetchStatusUp();
   const statusDown = await fetchStatusDown();
   return (
-    <main className="pt-20 static h-screen">
-      <Maps className={'pt-20'}>
-        {/* Status Up */}
-        <div className="flex flex-row gap-2 w-[16rem] z-[1000] absolute top-4 inset-x-0 mx-auto">
-          <div className="bg-green-500 py-2 w-1/2  rounded-xl flex flex-row justify-between">
-            <div className="px-3">
-              <div className="text-white text-xs">Status Up</div>
-              <div className="text-white text-lg">{statusUp[0].total}</div>
+    <main className="pt-20 static h-svh">
+      <div className='h-full'>
+        <Maps className={'pt-20'}>
+          {/* Status Up */}
+          <div className="flex flex-row gap-2 w-[16rem] z-[1000] absolute top-4 inset-x-0 mx-auto">
+            <div className="bg-green-500 py-2 w-1/2  rounded-xl flex flex-row justify-between">
+              <div className="px-3">
+                <div className="text-white text-xs">Status Up</div>
+                <div className="text-white text-lg">{statusUp[0].total}</div>
+              </div>
+              <div className="w-1/4 flex items-center justify-center border-l border-white">
+                {Icons.up}
+              </div>
             </div>
-            <div className="w-1/4 flex items-center justify-center border-l border-white">
-              {Icons.up}
-            </div>
-          </div>
 
-          {/* Status Down */}
-          <div className="bg-red-500 py-2 w-1/2  rounded-xl flex flex-row justify-between">
-            <div className="px-3">
-              <div className="text-white text-xs">Status Down</div>
-              <div className="text-white text-lg">{statusDown[0].total}</div>
-            </div>
-            <div className="w-1/4 flex items-center justify-center border-r rotate-180 border-white">
-              {Icons.up}
+            {/* Status Down */}
+            <div className="bg-red-500 py-2 w-1/2  rounded-xl flex flex-row justify-between">
+              <div className="px-3">
+                <div className="text-white text-xs">Status Down</div>
+                <div className="text-white text-lg">{statusDown[0].total}</div>
+              </div>
+              <div className="w-1/4 flex items-center justify-center border-r rotate-180 border-white">
+                {Icons.up}
+              </div>
             </div>
           </div>
-        </div>
-      </Maps>
+        </Maps>
+      </div>
     </main>
   );
 }

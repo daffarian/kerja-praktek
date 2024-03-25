beacfdb85d3ddabcb17f9070d9f8ae9e1421e417
daffarian <mdaffarian123@gmail.com>
2024-03-25 23:20:13 +0700

    safsd

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,16 +9,16 @@ export default async function Page() {
   const statusUp = await fetchStatusUp();
   const statusDown = await fetchStatusDown();
   return (
-    <main className="pt-20  h-screen flex flex-col">
+    <main className="pt-20 relative  h-screen flex flex-col">
       <Maps className={''}>
         {/* Status Up */}
-        <div className="flex flex-row gap-2 w-[20rem] z-[1000] absolute top-8 inset-x-0 mx-auto">
+        <div className="flex flex-row gap-2 w-[16rem] z-[1000] absolute top-4 inset-x-0 mx-auto">
           <div className="bg-green-500 py-2 w-1/2  rounded-xl flex flex-row justify-between">
             <div className="px-3">
-              <div className="text-white text-sm">Status Up</div>
-              <div className="text-white text-2xl">{statusUp[0].total}</div>
+              <div className="text-white text-xs">Status Up</div>
+              <div className="text-white text-lg">{statusUp[0].total}</div>
             </div>
-            <div className="w-1/3 flex items-center justify-center border-l border-white">
+            <div className="w-1/4 flex items-center justify-center border-l border-white">
               {Icons.up}
             </div>
           </div>
@@ -26,10 +26,10 @@ export default async function Page() {
           {/* Status Down */}
           <div className="bg-red-500 py-2 w-1/2  rounded-xl flex flex-row justify-between">
             <div className="px-3">
-              <div className="text-white text-sm">Status Down</div>
-              <div className="text-white text-2xl">{statusDown[0].total}</div>
+              <div className="text-white text-xs">Status Down</div>
+              <div className="text-white text-lg">{statusDown[0].total}</div>
             </div>
-            <div className="w-1/3 flex items-center justify-center border-r rotate-180 border-white">
+            <div className="w-1/4 flex items-center justify-center border-r rotate-180 border-white">
               {Icons.up}
             </div>
           </div>
